2d5a7c6c94bfa5c43baca7d897aa2d20cc484cf4
senbet22 <152603405+senbet22@users.noreply.github.com>
2025-05-12 14:14:02 +0700

    Update Product.tsx

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -40,10 +40,14 @@ const ProductPage: React.FC = () => {
   const discountPercentage = Math.round((discount / product.price) * 100);
 
   return (
-    <div className="container mx-auto p-4 max-w-5xl">
+    <div className="container mx-auto p-4 min-h-svh max-w-5xl">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {/* Image */}
-        <img src={product.image.url} alt={product.image.alt} className="w-full" />
+        <img
+          src={product.image.url}
+          alt={product.image.alt}
+          className="w-full"
+        />
 
         {/* Details */}
         <div>
@@ -56,9 +60,15 @@ const ProductPage: React.FC = () => {
           <div className="my-3">
             {discount > 0 ? (
               <>
-                <span className="font-bold">${product.discountedPrice.toFixed(2)}</span>
-                <span className="ml-2 line-through">${product.price.toFixed(2)}</span>
-                <span className="ml-2 bg-red-500 text-white px-2 py-1 text-xs">{discountPercentage}% OFF</span>
+                <span className="font-bold">
+                  ${product.discountedPrice.toFixed(2)}
+                </span>
+                <span className="ml-2 line-through">
+                  ${product.price.toFixed(2)}
+                </span>
+                <span className="ml-2 bg-red-500 text-white px-2 py-1 text-xs">
+                  {discountPercentage}% OFF
+                </span>
               </>
             ) : (
               <span className="font-bold">${product.price.toFixed(2)}</span>
@@ -90,15 +100,24 @@ const ProductPage: React.FC = () => {
                 id="quantity"
                 min="1"
                 value={quantity}
-                onChange={(e) => setQuantity(Math.max(1, parseInt(e.target.value) || 1))}
+                onChange={(e) =>
+                  setQuantity(Math.max(1, parseInt(e.target.value) || 1))
+                }
                 className="border w-16 px-2 py-1"
               />
             </div>
-            <BaseButton className="py-2 px-4 flex items-center" onClick={onAddToCartClick}>
+            <BaseButton
+              className="py-2 px-4 flex items-center"
+              onClick={onAddToCartClick}
+            >
               <FaShoppingCart className="mr-2" />
               Add to Cart
             </BaseButton>
-            {hasItem(product.id.toString()) && <div className="mt-2 text-sm text-green-600">Item already in cart.</div>}
+            {hasItem(product.id.toString()) && (
+              <div className="mt-2 text-sm text-green-600">
+                Item already in cart.
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -106,12 +125,25 @@ const ProductPage: React.FC = () => {
       {/* Reviews */}
       {product.reviews && product.reviews.length > 0 && (
         <div className="mt-8">
-          <div className="flex align-baseline gap-4 cursor-pointer" onClick={toggleReviews}>
+          <div
+            className="flex align-baseline gap-4 cursor-pointer"
+            onClick={toggleReviews}
+          >
             <h2 className="text-xl font-bold mb-4 ">Customer Reviews </h2>
-            <span className="text-gray-400 font-medium">({product.reviews.length})</span>
-            {isVisible ? <IoIosArrowUp size={35} /> : <IoIosArrowDown size={35} />}
+            <span className="text-gray-400 font-medium">
+              ({product.reviews.length})
+            </span>
+            {isVisible ? (
+              <IoIosArrowUp size={35} />
+            ) : (
+              <IoIosArrowDown size={35} />
+            )}
           </div>
-          <div className={`grid grid-cols-1 md:grid-cols-2 gap-4 ${isVisible ? "grid" : "hidden"}`}>
+          <div
+            className={`grid grid-cols-1 md:grid-cols-2 gap-4 ${
+              isVisible ? "grid" : "hidden"
+            }`}
+          >
             {product.reviews.map((review) => (
               <div key={review.id} className="border p-4">
                 <div className="flex justify-between mb-2">
