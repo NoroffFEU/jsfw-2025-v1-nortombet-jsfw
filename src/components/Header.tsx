5167dddad77e43083247ff0efe611e24e628bb90
senbet22 <152603405+senbet22@users.noreply.github.com>
2025-05-15 21:12:28 +0700

    Mobile navbar styling fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,7 @@ const Header = () => {
   }, [mobileMenu]);
 
   return (
-    <header className="bg-[#574964] text-white p-2 sticky top-0 left-0 right-0 z-50">
+    <header className="bg-[#574964] text-white md:p-2 sticky top-0 left-0 right-0 z-50">
       <nav className="h-[60px] flex items-center justify-between bg-secondary max-w-6xl mx-auto">
         <div>
           <img
@@ -89,10 +89,14 @@ const Header = () => {
           </BaseButton>
         </div>
 
-        <BiMenuAltRight size={40} className="mr-4 md:hidden" onClick={toggleMobileMenu} />
+        <BiMenuAltRight
+          size={40}
+          className="mr-4 md:hidden"
+          onClick={toggleMobileMenu}
+        />
 
         {mobileMenu && (
-          <div className="md:hidden text-black fixed w-full top-0 bottom-0 z-40 overflow-hidden bg-white transition-all flex flex-col">
+          <div className="md:hidden text-black fixed w-full top-0 bottom-0 z-40 overflow-hidden bg-[#FDD8B3] transition-all flex flex-col">
             <div className="flex justify-end p-4">
               <RiCloseLargeFill size={40} onClick={toggleMobileMenu} />
             </div>
